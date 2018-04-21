0ec714043579f9c4dd9e01e8e42bb5cc5199193f
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2018-04-21 11:33:14 -0500

    creating first html

--- instagram/settings.py
+++ instagram/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'services/static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'services/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'services/static/images')
 
