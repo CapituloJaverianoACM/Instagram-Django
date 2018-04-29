1c4cd10b58e87658c3e08f1c71f681543e0c61bf
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2018-04-29 15:39:35 -0500

    creating index and login page

--- services/views.py
+++ services/views.py
@@ -1,5 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
 	return render(request, "index.html");
+
+def login(request):
+	return render(request, "login.html");
+
+def register(request):
+	print(request)
+	return HttpResponse("Hola");
+
+def validateUser(request):
+	print(request)
+	return HttpResponse("Hola");
