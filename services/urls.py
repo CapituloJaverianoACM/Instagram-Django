c2e80ae08635545f516439066ee08809c500a56a
Ático <atico@202CS1E01164403.local>
2018-05-19 12:34:43 -0500

    Profile backend

--- services/urls.py
+++ services/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login/', views.login, name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name="login"),
     path('home/', views.home, name="home"),
     path('profile/', views.profile, name="profile"),
-
     path('register/', views.register, name="register"),
-    path('loginuser/', views.validate_user, name="validate"),
+    path('loginuser/', views.login, name="validate"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
