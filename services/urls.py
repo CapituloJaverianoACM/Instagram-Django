37495dce393e6c32a8487eb6b0e9a6835cf69aa9
= <=>
2018-05-26 13:03:04 -0500

    Upload photo backend. Search users by username. Follow and unfollow

--- services/urls.py
+++ services/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name="login"),
     path('home/', views.home, name="home"),
-    path('profile/', views.profile, name="profile"),
+    path('profile/<str:username>', views.profile, name="profile"),
     path('register/', views.register, name="register"),
     path('loginuser/', views.login, name="validate"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('upload/', views.upload_photo, name="upload"),
+    path('search/', views.search, name="search"),
+    path('follow/<str:begin>/<str:end>', views.follow, name="follow"),
+    path('unfollow/<str:begin>/<str:end>', views.unfollow, name="unfollow"),
 ]
--- services/views.py
+++ services/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
 
 # Models
 from django.contrib.auth.models import User
@@ -19,24 +20,55 @@ def login(request):
 def home(request):
     if not request.user.is_authenticated:
         return redirect('index')
-    return render(request, "home.html")
+    context = dict(username=request.user.username)
+    return render(request, "home.html", context)
+
 
 def upload_photo(request):
     if not request.user.is_authenticated:
         return redirect('index')
-    return render(request, "upload.html")
+    if request.method == 'GET':
+        context = dict(username=request.user.username)
+        return render(request, "upload.html", context)
+    if request.method == 'POST':
+        username = request.user.username
+        my_user = request.user.myuser
+        last_post = Post.objects.filter(user=my_user).last()
+        if last_post:
+            photo_id = last_post.id + 1
+        else:
+            photo_id = 1
+        description = request.POST['description']
+        photo = request.FILES['file']
+        photo_name = username + '_photo_' + str(photo_id)
+        fs = FileSystemStorage()
+        url_photo = fs.save(photo_name, photo)
+        path_photo = fs.url(url_photo)
 
-def profile(request):
+        post = Post.objects.create(photo=path_photo, description=description, user=my_user)
+        return redirect('profile')
+
+
+def profile(request, username):
     if not request.user.is_authenticated:
         return redirect('index')
-    user = request.user
+
+    context = dict()
+    if username == request.user.username:
+        user = request.user
+    else:
+        user = User.objects.get(username=username)
+        is_follow = Follow.objects.filter(user_from=request.user.myuser, user_to=user.myuser).exists()
+        context.update(is_follow=is_follow)
+
     my_user = MyUser.objects.get(user_django=user)
     my_user_id = my_user.id
     post = Post.objects.filter(user_id=my_user_id)
     number_followers = Follow.objects.filter(user_to_id=my_user_id).count()
     number_victims = Follow.objects.filter(user_from_id=my_user_id).count()
-    context = dict(
-        username=user.username,
+    context.update(
+        username=request.user.username,
+        current_username=user.username,
         post=post,
         number_followers=number_followers,
         number_victims=number_victims,
@@ -62,3 +94,27 @@ def register(request):
 
 def validate_user(request):
     return redirect('home')
+
+
+def search(request):
+    username = request.POST['search_username']
+    user = User.objects.get(username=username)
+    if user:
+        return redirect('profile', user.username)
+    else:
+        return redirect('profile', request.user.username)
+
+
+def follow(request, begin, end):
+    user_begin = User.objects.get(username=begin)
+    user_end = User.objects.get(username=end)
+    Follow.objects.create(user_from=user_begin.myuser, user_to=user_end.myuser)
+    return redirect('profile', end)
+
+
+def unfollow(request, begin, end):
+    user_begin = User.objects.get(username=begin)
+    user_end = User.objects.get(username=end)
+    follow = Follow.objects.get(user_from=user_begin.myuser, user_to=user_end.myuser)
+    follow.delete()
+    return redirect('profile', end)
